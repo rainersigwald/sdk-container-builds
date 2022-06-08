883734e309a55b327583345a08f0f084f3cc18c1
Rainer Sigwald <raines@microsoft.com>
2022-06-08 10:47:15 -0500

    WIP: get an image manifest from a registry

---
+++ System.Containers/Image.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Containers;
+
+public class Image
+{
+    public int schemaVersion { get; set; }
+    public Descriptor Config { get; set; }
+    public Descriptor[] Layers { get; set; }
+}
+
+    public class Config
+    {
+        public string? mediaType { get; set; }
+        public int size { get; set; }
+        public string digest { get; set; }
+    }
---
+++ System.Containers/Registry.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace System.Containers;
+
+public record struct Registry(Uri BaseUri)
+{
+    public async Task GetImageManifest(string name, string reference)
+    {
+        using HttpClient client = new(new HttpClientHandler() { UseDefaultCredentials = true });
+        
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/vnd.docker.distribution.manifest.v2+json"));
+
+        client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+        var response = await client.GetAsync(new Uri(BaseUri, $"/v2/{name}/manifests/{reference}"));
+
+        var s = await response.Content.ReadAsStringAsync();
+
+        Image? image = JsonSerializer.Deserialize<Image>(s);
+    }
+}
